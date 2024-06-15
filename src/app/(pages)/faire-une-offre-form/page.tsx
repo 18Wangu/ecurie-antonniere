7aeeed53cc67a6c9929a698613c7a164bbae11e0
Theo <theopremartin@orange.fr>
2024-06-15 17:19:40 +0200

    frontend du formulaire version 1 fini

--- src/app/(pages)/faire-une-offre-form/page.tsx
+++ src/app/(pages)/faire-une-offre-form/page.tsx
@@ -8,7 +8,6 @@ import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -16,18 +15,31 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { toast } from "@/components/ui/use-toast"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  horseName: z.string().nonempty({
+    message: "Horse name is required.",
   }),
+  number: z.preprocess((val) => Number(val), z.number().min(1, {
+    message: "Veuillez rentrer un prix",
+  })),
+  message: z.string().optional(),
 })
 
 export default function InputForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: "",
+      horseName: "",
+      number: "",
+      message: "",
     },
   })
 
@@ -43,26 +55,61 @@ export default function InputForm() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="flex justify-center items-center flex-col h-screen">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+          <FormField
+            control={form.control}
+            name="horseName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nom du cheval</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Nom du cheval" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="tonere">Tonere</SelectItem>
+                    <SelectItem value="eclair">Eclair</SelectItem>
+                    <SelectItem value="tempete">Tempete</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Proposition prix</FormLabel>
+                <FormControl>
+                  <Input type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Input type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Envoyer</Button>
+        </form>
+      </Form>
+    </div>
   )
 }
